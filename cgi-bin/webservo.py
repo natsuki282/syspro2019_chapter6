8bc9d5223a4b880c4515a6b4277f3bf39f8973b5
natsuki282 <natsuki.kb.282@gmail.com>
2019-12-10 10:58:03 +0900

    servo

--- cgi-bin/webservo.py
+++ cgi-bin/webservo.py
@@ -10,17 +10,24 @@ print('Content-type: text/html; charset=UTF-8\r\n')
 print('Web SERVO')
 
 print('<form action="" method="post">')
-print('<input type="text" name="servo">')
+print('<input type="number" name="servo" min="-90" max="90">')
 print('<input type="submit" name="turnbutton" value="TURN">')
 print('</form>')
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2,GPIO.OUT)
+servo=GPIO.PWM(2,50)
+servo.start(0.0)
+
 
 form=cgi.FieldStorage()
 rand=form.getvalue("servo")
+value=form.getvalue("turnbutton")
 
-ang=((0.0105*(rand+90.0))+0.5)*5.0
-servo.ChangeDutyCycle(ang)
-time.sleep(1.0)
-GPIO.cleanup()
+
+if value=="TURN":
+	ang=((0.0105*(float(rand)+90.0))+0.5)*5.0
+	servo.ChangeDutyCycle(ang)
+	time.sleep(1.0)
+
+	GPIO.cleanup()
